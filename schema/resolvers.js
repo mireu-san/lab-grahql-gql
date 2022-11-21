d25d942b6badafb29f898853a435d70a7d4ee9ec
Chesed Kim <starmireu@gmail.com>
2022-11-21 17:34:44 +0900

    saved - treating Resolvers

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,36 @@
-const { UserList } = require("../FakeData");
+const { UserList, MovieList } = require("../FakeData");
+const _ = require("lodash");
 
 const resolvers = {
+  // User resolvers
   Query: {
-    users() {
+    users: () => {
       return UserList;
+    },
+    user: (parent, args) => {
+      const id = args.id;
+      // .find(what do u want to find out, { key })
+      // : Number(id) -> only did this to convert string to number.
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
+    },
+    // Movie resolvers
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name });
+      return movie;
+    },
+  },
+  User: {
+    favoriteMovies: () => {
+      return _.filter(
+        MovieList,
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
     },
   },
 };
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -2,18 +2,40 @@ const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type User {
+    id: ID!
     name: String!
     username: String!
     age: Int!
     nationality: String!
+    # do not put exclamation mark(meant required) here on purpose - some of them do not wish to mark this info.
+    friends: [User]
+    favoriteMovies: [Movie]
+  }
+
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
   }
 
   type Query {
     users: [User!]!
+    user(id: ID!): User!
+    movies: [Movie!]!
+    movie(name: String!): Movie!
+  }
+
+  enum Nationality {
+    CANADA
+    BRAZIL
+    INDIA
+    GERMANY
+    CHILE
   }
 `;
 
+module.exports = { typeDefs };
+
 // https://graphql.org/learn/schema/#:%7E:text=means%20that%20the%20field%20is,those%20with%20an%20exclamation%20mark
 // String! means that the field is non-nullable, meaning that the GraphQL service promises to always give you a value when you query this field. In the type language, we'll represent those with an exclamation mark.
-
-module.exports = { typeDefs };
