7c0cfb493bb8a38b8f1068c0a96fc48d3f2daf9e
Chesed Kim <starmireu@gmail.com>
2022-11-21 19:51:08 +0900

    mutation success

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -43,6 +43,27 @@ const resolvers = {
       UserList.push(user);
       return user;
     },
+
+    // this name must be same as the one how I defined from type-def.js (updateUsername)
+    updateUsername: (parent, args) => {
+      // const id = args.input.id;
+      // const newUsername = args.input.newUsername
+      const { id, newUsername } = args.input;
+      let userUpdated;
+      UserList.forEach((user) => {
+        if (user.id === Number(id)) {
+          user.username = newUsername;
+          userUpdated = user;
+        }
+      });
+      return userUpdated;
+    },
+
+    deleteUser: (parent, args) => {
+      const id = args.id;
+      _.remove(UserList, (user) => user.id === Number(id));
+      return null;
+    },
   },
 };
 
