202d19e574fba412530afd8f6cdab2f2075a2035
Chesed Kim <starmireu@gmail.com>
2022-11-22 23:08:23 +0900

    react - added DisplayData, to use useQuery hook, @apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import DisplayData from "./DisplayData";
 
 function App() {
   const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: "http://localhost:3001/graphql",
+    uri: "http://localhost:4000/graphql",
   });
 
   return (
     <ApolloProvider client={client}>
-      <div className="App">Test</div>
+      <div className="App">
+        <DisplayData />
+      </div>
     </ApolloProvider>
   );
 }
---
+++ client/src/DisplayData.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+
+function DisplayData() {
+  const {} = useQuery();
+
+  return <div>DisplayData</div>;
+}
+
+export default DisplayData;
