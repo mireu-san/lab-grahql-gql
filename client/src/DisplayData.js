86545bdd2e4baa77a3c4efabc72e18fe2708a413
Chesed Kim <starmireu@gmail.com>
2022-11-22 23:31:22 +0900

    useQuery - querying specific data as value

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -9,19 +9,64 @@ const QUERY_ALL_USERS = gql`
       name
       age
       username
+      nationality
+    }
+  }
+`;
+
+const QUERY_ALL_MOVIES = gql`
+  query GetAllUsers {
+    movies {
+      name
     }
   }
 `;
 
 function DisplayData() {
   // useQuery will make request
-  const { data } = useQuery(QUERY_ALL_USERS);
+  const { data, loading, error } = useQuery(QUERY_ALL_USERS);
+  const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
+
+  if (loading) {
+    return <h1> 데이터가 로드 중 입니다. 메시지 출력중...</h1>;
+  }
 
   if (data) {
     console.log(data);
   }
 
-  return <div></div>;
+  if (error) {
+    console.log(error);
+  }
+
+  if (data) {
+    console.log(data);
+  }
+
+  return (
+    <div>
+      {/* Users */}
+      {/* if data is available, then map the data */}
+      {data &&
+        // data.map((user) => {
+        data.users.map((user) => {
+          return (
+            <div>
+              <h1>이름: {user.name}</h1>
+              <h1>유저이름: {user.username}</h1>
+              <h1>나이: {user.age}</h1>
+              <h1>국적: {user.nationality}</h1>
+            </div>
+          );
+        })}
+
+      {/* Movies */}
+      {movieData &&
+        movieData.movies.map((movie) => {
+          return <h1>이름: {movie.name}</h1>;
+        })}
+    </div>
+  );
 }
 
 export default DisplayData;
